88c53448b23058f43c4b2e2f7e3893ae2213fed3
Brian Ochieng Otieno <brianochieng1989@gmail.com>
2023-09-18 03:16:07 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(a,b){
+    return a+b;
+}
+function subtract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    return a/b;
+}
+n=5;
+function increment(n){
+        return (n +=1);
+}
+n=6
+function decrement(n){
+    return (n -=1);
+}
+n="20"
+function  makeInt(n){
+    return parseInt(n, 10)
+}
+n="10.55556"
+function preserveDecimal(n){
+    return parseFloat(n)
+}
